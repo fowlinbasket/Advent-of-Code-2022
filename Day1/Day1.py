75b181105cd0d2030f9364138f2dadff44a3aca1
fowlinbasket <foul.in.basket@gmail.com>
2022-12-01 19:00:59 -0700

    Finished Day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,44 @@
+import sys
+from queue import PriorityQueue
+
+class Elf:
+    def __init__(self, totalCalories=0):
+        self.__totalCalories = totalCalories
+
+    def addCalories(self, calories):
+        self.__totalCalories += calories
+
+    def getTotalCalories(self):
+        return self.__totalCalories
+
+    def __lt__(self, other):
+        # This is for the PriorityQueue
+        return self.getTotalCalories() > other.getTotalCalories()
+
+def getHighestCalories(filePath):
+    f = open(filePath, "r")
+    elves = PriorityQueue()
+    elf = Elf()
+    for line in f.readlines():
+        line = line.strip("\n")
+        if len(line) == 0:
+            elves.put(elf)
+            elf = Elf()
+            continue
+        else:
+            elf.addCalories(int(line))
+    return elves
+
+def puzzle1(filePath):
+    return getHighestCalories(filePath).get().getTotalCalories()
+
+def puzzle2(filePath):
+    totalCalories = 0
+    elves = getHighestCalories(filePath)
+    for _ in range(3):
+        totalCalories += elves.get().getTotalCalories()
+    return totalCalories
+
+if __name__ == '__main__':
+    print(f"The answer to Day 1 Puzzle 1 is {puzzle1(sys.argv[1])}")
+    print(f"The answer to Day 1 Puzzle 2 is {puzzle2(sys.argv[1])}")
